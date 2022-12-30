db3f60f4023667a2f5ccee56e5eb9b6cab47c14e
NajhelyBS <s5anbandasanchez@gmail.com>
2022-12-30 11:03:32 -0500

    I am going to star with the condition, name, last name and age work

--- src/app/datos/datos.component.ts
+++ src/app/datos/datos.component.ts
@@ -9,13 +9,16 @@ export class DatosComponent {
 
   grupo1 = ["  "];
   grupo2 = ["  "];
- 
+  grupo3 = ["  "];
 
-  Registro(nombre:String,apellidos:String){
+  Registro(nombre:String,apellidos:String,edad:Number){
     this.grupo1.push(String(nombre));
     this.grupo2.push(String(apellidos));
-
+    this.grupo3.push(String(edad));
     alert(nombre);
     alert(apellidos);
+    alert(edad);
+
+    
   }
 }
